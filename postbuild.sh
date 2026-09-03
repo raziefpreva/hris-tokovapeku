053683b78bbf2d3fffad6a6695de9be8daf70b56
Razief Azis <raziefazis@Raziefs-MacBook-Air.local>
2026-09-03 23:54:47 +0700

    fix: dynamically resolve bundle script in postbuild

--- postbuild.sh
+++ postbuild.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 mkdir -p dist/client
-echo '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"/><meta name="viewport" content="width=device-width, initial-scale=1.0"/><title>HRIS Tokovapeku</title></head><body><div id="root"></div><script type="module" src="/assets/app.js"></script></body></html>' > dist/client/index.html
+MAIN_JS=$(ls dist/client/assets/bootstrap-*.js dist/client/assets/index-*.js 2>/dev/null | head -n 1 | xargs -n 1 basename)
+echo "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"/><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/><title>HRIS Tokovapeku</title></head><body><div id=\"root\"></div><script type=\"module\" src=\"/assets/${MAIN_JS}\"></script></body></html>" > dist/client/index.html
