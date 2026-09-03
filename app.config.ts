e962c0bd44ef60e34ae232038afee78f5232bcc0
Razief Azis <raziefazis@Raziefs-MacBook-Air.local>
2026-09-03 15:13:40 +0700

    fix permanent static build for vercel

--- app.config.ts
+++ app.config.ts
@@ -1,1 +1,1 @@
-import { defineConfig } from "@tanstack/start/config"; export default defineConfig({ server: { preset: "vercel" } });
+import { defineConfig } from "@tanstack/start/config"; export default defineConfig({ deployment: { target: "static" } });
