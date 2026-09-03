727f591201660b1caaf17e874f201479d6c34579
Razief Azis <raziefazis@Raziefs-MacBook-Air.local>
2026-09-03 18:32:27 +0700

    fix: cleanup app.config.ts and configure vite with vercel preset

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,7 @@ export default defineConfig({
   plugins: [
     tsConfigPaths(),
     tanstackStart({
-      server: {
-        entry: "src/server.ts",
-      },
+      preset: "vercel",
     }),
     react(),
   ],
