e36ad0193fab184955e113c5b9a968a7cb01c43f
Razief Azis <raziefazis@Raziefs-MacBook-Air.local>
2026-09-04 00:48:01 +0700

    fix: move postbuild actions to safe shell script

--- postbuild.sh
+++ postbuild.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-# Pastikan index.html standar dari hasil build client disalin dengan benar ke root dist/client
-if [ -f "dist/client/index.html" ]; then
-  cp dist/client/index.html dist/client/404.html
+if [ -d "dist/client" ]; then
+  if [ -f "dist/client/index.html" ]; then
+    cp dist/client/index.html dist/client/404.html
+  fi
 fi
 echo "/*    /index.html   200" > dist/client/_redirects
